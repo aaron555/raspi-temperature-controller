b3e198b79b68d51059e6f127c3c564a3db87f9c3
Aaron Lockton <aaronlockton@yahoo.co.uk>
2020-05-10 20:42:40 +0100

    First commit of working versions of controller and analysis scripts, controller is working but WIP, controller analyse is current running version on heating system

---
+++ scripts/control_temp.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python3
+
+# Simple binary temperature controller with configurable hysterisis and control and data loggins
+
+# INPUTS:
+# <Setpoint> must be specified - may be either a value or a string containing path to a file containing the required value
+# All other input arguments are optional
+# ./control_temp.py -h for a list of supported input arguments
+
+# OUTPUTS: Demand signal to GPIO; Summary of system status and any change in status to STDOUT; timestamped logfile with setpoint, measured temperature and status
+
+# EXAMPLE CALL
+# ./control_temp.py 27 >> /home/aaron/control_temp.log
+
+# NOTE if multiple temperature sensors (--sensorid) are specified, the first sensor in the list will always be used for control, the others will just be read and logged
+# If labels (--label) are also specified, the number of labels specified must match the number of sensors (--sensorid)
+# For multi-channel temperature control, run a separate instance of this script for each channel, specifying appropriate temperature sensor input and GPIO output, logfile (optionally channel name) for each
+
+# Changelog
+# 04/11/4014 - First Version
+# 05/2020 - Removed hard-coded inputs and changed to arguments, changed default logging to CSV, added python3 compatibility, added optional continuous mode with configurable cycle interval
+
+# Copyright (C) 2014, 2020 Aaron Lockton
+
+import sys
+import os
+import glob
+from time import sleep, gmtime, strftime, time
+import argparse
+
+# Debug messages (verbose mode)
+def verbose_print(message):
+  if args.verbose:
+    print("%s: DEBUG: %s" % (strftime("%Y-%m-%d-%H:%M:%S", gmtime()), message))
+
+def format_print(message):
+  print("%s: %s" % (strftime("%Y-%m-%d-%H:%M:%S", gmtime()), message))
+
+# Configure GPIO specified and set direction specified, if not already configured
+def configure_gpio(gpionum,direction):
+  if direction != "in" and direction != "out":
+    return None
+  gpionum = str(gpionum)
+  # Export GPIO to allow it to be used
+  if not os.path.exists('/sys/class/gpio/gpio'+gpionum+'/'):
+    verbose_print("GPIO "+gpionum+" is not configured - exporting")
+    os.system('echo '+gpionum+' > /sys/class/gpio/export')
+  # Set GPIO direction
+  with open('/sys/class/gpio/gpio'+gpionum+'/direction', 'r') as f:
+    current_direction = f.readline().rstrip()
+    verbose_print("Current GPIO "+gpionum+" setting: " + current_direction)
+  if current_direction != direction:
+    # If direction is not correct, may have only just been exported, need delay to prevent failure due to first-run permissions issue in Raspbian
+    sleep(1)
+    verbose_print("Setting GPIO "+gpionum+" direction to "+direction)
+    os.system('echo '+direction+' > /sys/class/gpio/gpio'+gpionum+'/direction')
+
+# Read GPIO value from specified GPIO
+def get_gpio(gpionum):
+  gpionum = str(gpionum)
+  with open('/sys/class/gpio/gpio'+gpionum+'/value', 'r') as f:
+    status = int(f.readline())
+    return status
+
+# Set specified GPIO to specified value
+def set_gpio(gpionum,value):
+  gpionum = str(gpionum)
+  if value != "0" and value != "1":
+    return None
+  os.system('echo '+value+' > /sys/class/gpio/gpio'+gpionum+'/value')
+
+# Read temperature from 1-wire sensor on GPIO7 -returns None on error, or the temperature as a float
+def get_temp(devicefile):
+  try:
+    with open(devicefile, 'r') as f:
+      lines = f.readlines()
+  except:
+    return None
+
+   # get the status from the end of line 1
+  status = lines[0][-4:-1]
+
+   # is the status is ok, get the temperature from line 2
+  if status=="YES":
+    tempstr =  lines[1].rsplit("t=")[1]
+    tempvalue=float(tempstr)/1000
+    return tempvalue
+  else:
+    return None
+
+# Parse input arguments
+parser = argparse.ArgumentParser(description='Simple Temperature Controller.')
+parser.add_argument('setpoint', type=str,
+  help='Setpoint Temperature (C) or path to file containing setpoint')
+parser.add_argument('--hysteresis', '-t', type=float, default=0.1, metavar='FLOAT(C)',
+  help='Hystersis between switch-off and switch on (C) - default: 0.1')
+parser.add_argument('--cooler', '-c', action='store_true',
+  help='By default assume controlling heater - set this flag for cooler (invert output / move hysteresis above setpoint)')
+parser.add_argument('--sensorid', '-s', type=str, nargs='+', metavar='STRING(S)',
+  help='1-wire temperature sensor ID e.g. "28-xxxx" (default: first of /sys/bus/w1/devices/28-*/w1_slave detected) - NOTE if multiple temperature sensors specified, first sensor in list will be used for control')
+parser.add_argument('--label', '-n', type=str, nargs='+', metavar='STRING(S)',
+  help='Channel label/name prefix for temperature sensor(s) in log header - default: "Current"')
+parser.add_argument('--gpioout', '-g', type=int, default=17, metavar='GPIO',
+  help='GPIO pin for output heating demand signal - default: GPIO17 / Pin 11 (integer)')
+parser.add_argument('--gpiofeedback', '-f', type=int, metavar='GPIO',
+  help='GPIO pin for feedback signal from heater - default same as --gpioout (integer)')
+parser.add_argument('--logfile', '-l', type=str, metavar='FILENAME', default="temperature_data.csv",
+  help='Full path and filename of output logfile for temperature and setpoint data - default: "temperature_data.csv" (string)')
+parser.add_argument('--legacylog', '-y', action='store_true',
+  help='Legacy logging mode - if this flag is set uses legacy logfile format instead of default: Excel-friendly CSV')
+parser.add_argument('--interval', '-i', type=float, metavar='SECONDS',
+  help='Interval between control cycle (s) - specify to enable continuous mode - default: run once and exit')
+parser.add_argument('--verbose', '-v', action='store_true',
+  help='Verbose mode - if this flag is set additional messages of control process sent to STDOUT - useful for debugging')
+args = parser.parse_args()
+# *** check all have correct datatype validation and defaults;
+
+# Check input argumants, set defaults where necessaru and validate
+setarg = args.setpoint
+if str.isdigit(setarg[0]):
+  # If first character of specified setpoint is a number, use directly
+  setpoint = float(setarg)
+else:
+  # If first character of specified setpoint is not a number, assume it is a file path
+  with open(setarg, 'r') as f:
+    setpoint = float(f.readline())
+
+hysteresis = args.hysteresis
+
+if args.sensorid:
+  # set temp_sensor(s) to specified list - handle errors later when list iterated
+  temp_sensors = args.sensorid
+else:
+  # Find list of /sys/bus/w1/devices/28-*/w1_slave and select first
+  sensor_list = glob.glob('/sys/devices/w1_bus_master1/28*/w1_slave')
+  temp_sensors = [sensor_list[0].split('/')[4]]
+# *** note temp sensor on heating controller is 28-0000050399c6
+
+if args.label:
+  temp_labels = args.label
+else:
+  if len(temp_sensors) == 1:
+    temp_labels = ["Current"]
+  else:
+    temp_labels=temp_sensors
+
+if len(temp_labels) != len(temp_sensors):
+  format_print("ERROR: Number of label(s) (--label) must match number of sensor(s) (--sensorid) if both arguments are specified")
+  sys.exit()
+
+gpio_output = args.gpioout
+
+# ***note wiringpi gpio3 on heating controller is gpio22
+if args.gpiofeedback:
+  gpio_feedback = args.gpiofeedback
+else:
+  gpio_feedback = gpio_output
+
+# *** Note logfile on heating controller /var/log/xrf_modules/xrf_poll_cont.log
+logfile_fullpath = args.logfile
+
+cycle_interval = args.interval
+
+# *** DEBUG
+print(args)
+verbose_print("Setpoint: "+str(setpoint)+"  Hysteresis: "+str(hysteresis)+"  Temperature sensor(s): "+','.join(temp_sensors)+"  Channel label(s): "+','.join(temp_labels))
+
+# Prepare CSV file header for data log
+CSV_header = "Timestamp,Setpoint (C),"
+for temp_label in temp_labels:
+  CSV_header += temp_label+" Temperature (C),"
+CSV_header += "Demand Status (0/1)\n"
+verbose_print("CSV header: "+CSV_header)
+
+# Set up GPIOs
+configure_gpio(gpio_output,"out")
+if gpio_output != gpio_feedback:
+  configure_gpio(gpio_feedback,"in")
+
+# Main loop - continues once per --interval seconds or if --interval is not set execcutes one cycle and exits
+while True:
+  # Get current temperature from sensor
+  current_temps = []
+  for temp_sensor in temp_sensors:
+    current_temps.append(get_temp('/sys/bus/w1/devices/'+temp_sensor+'/w1_slave'))
+    if current_temps[-1] == None:
+      format_print("ERROR: Cannot get current temperature from sensor "+temp_sensor+" - check 1-wire driver enabled, sensor is connected correctly and (if set) --sensorid is correct")
+      current_temps[-1] = ""
+  verbose_print("Current Temperatures: "+''.join(str(current_temps)))
+  # If multiple sensors, note first sensor specified is always used for control
+  current_temp = current_temps[0]
+  if current_temp == "":
+  # If error occurs on control channel it is critical error, otherwise ignore
+    if cycle_interval:
+      # In continuous mode, wait for next cycle and try again
+      sleep(cycle_interval)
+      continue
+    else:
+      sys.exit()
+
+  # Compare temperature with setpoint, set heating demand signal accordingly
+  # Note switch on below setpoint and off at setpoint works best for most heater controllers, since reaction to demand on tends to be faster than off
+  verbose_print("Comparing measured temperature and setpoint")
+  above = (current_temp + hysteresis) < setpoint
+  below = current_temp  > setpoint
+  status = get_gpio(gpio_output)
+  if above:
+    verbose_print("Heating required, checking if system is on")
+    if status == 0:
+      print("%s: Setpoint=%s, Actual=%s - Switching system on" % (strftime("%Y-%m-%d-%H:%M:%S", gmtime()), setpoint, current_temp))
+      set_gpio(gpio_output,"1")
+      status = 1
+  elif below:
+    verbose_print("Heating not required, checking if system is on")
+    if status == 1:
+      print("%s: Setpoint=%s, Actual=%s - Switching system off" % (strftime("%Y-%m-%d-%H:%M:%S", gmtime()), setpoint, current_temp))
+      set_gpio(gpio_output,"0")
+      status = 0
+  else:
+    verbose_print("Temperature OK")
+    pass
+
+  # Read back demand signal - if spare relay contacts (DP), can test here if relay has actually switched
+  # Else if additional GPIO for feedback not specified, check status of GPIO output matches demand
+  actual_status = get_gpio(gpio_feedback)
+
+  if actual_status != status:
+    print("ERROR: Requested demand status "+str(status)+" but actual status "+str(actual_status)+" - failed to set demand signal!")
+
+  # Write temperature, setpoint and actual status to log - Note all all timestamps in UTC
+  with open(logfile_fullpath,"a") as f:
+    if args.legacylog:
+      # Backwards compatibilty - Use previous message-style log - note does not support multiple temperature sensors
+      f.write("%s %d Setpoint: %s Actual: %s Status: %s \n" % (strftime("%Y-%m-%d-%H-%M-%S", gmtime()), time(), setpoint, current_temp, actual_status))
+    else:
+      # Write CSV file with Excel-friendly timestamp.  For CSV mode, write header if file does not exist
+      if os.stat(logfile_fullpath).st_size == 0:
+        f.write(CSV_header)
+      f.write("%s,%s,%s,%s\n" % (strftime("%Y-%m-%d %H:%M:%S", gmtime()), setpoint, ','.join(map(str, current_temps)), actual_status))
+
+  # Check if one-shot mode or continuous - if interval argument is set use continuous
+  if cycle_interval:
+    sleep(cycle_interval)
+  else:
+    break
+
+#   check all ***
+#   Implement --cooler - invert logic, swsp hysteresis
+#   move all DEBUG lines to verbose_print, and all other lines to format_print
+#   document outputs - log for controller_analyse (STDOUT), errors/warning (STDOUT), temperature to Excel-friendly CSV (or optional legacy message log for backwards compat)
+#   testing including ALL input argument combinations (set/not set/multiple/etc) to ensure correct state set (log analysis), data logged(CSV), excel plots, multi channel, clean reboot, fresh image, etc
---
+++ scripts/controller_analyse.py
@@ -0,0 +1,283 @@
+#!/usr/bin/env python3
+
+# Analyse temperature controller log-files produced by control_temp.py
+
+# Syntax: ./controller_analyse.py [<full filename and path of log> <start time> <end time>]
+
+# All arguments are optional
+# If <full filename and path of log> is not specified default /var/log/control_temp.log
+# If <start time> is not specified default [midnight at end of day on which first switching event occurs]
+# If <end time> is not specified default [midnight at start of last day in log]
+
+# Note start and end times MUST be either a string in the form YYYY-MM-DD or an integer unix timestamp
+# Invalid start or end times will be ignored (default of all available data used)
+# Note if the end date is later than end of log, it will be assumed system status is held from end of log to requested end
+
+# Outputs: CSV file with amount of time system on (in hours and %) for each day
+# Plot and bar chart of hours on each day
+
+# Changelog
+# 2015 - First Version
+# 19/04/2020 - Fixed bug with analysis of days after log ends, changed CSV to 2 d.p., added python3 compatibility
+
+# Copyright (C) 2015, 2020 Aaron Lockton
+
+print("Importing modules...")
+
+import time
+from time import gmtime, strftime
+import os
+import sys
+import calendar
+from matplotlib.dates import date2num, DAILY
+import datetime as DT
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+print(strftime("%Y-%m-%d-%H:%M:%S: Starting temperature controller log analysis", gmtime()))
+# Set defaults
+if len(sys.argv) < 2:
+  log_file = "/var/log/control_temp.log"
+else:
+  log_file = sys.argv[1]
+  if os.path.isfile(log_file) != 1:
+    print("WARNING: Cannot find log file specified - using default")
+    log_file = "/var/log/control_temp.log"
+
+if len(sys.argv) < 3:
+  requested_start = 0
+else:
+  requested_start_raw = sys.argv[2]
+  if str.isdigit(requested_start_raw):
+    requested_start = int(requested_start_raw)
+  else:
+    try:
+      requested_start = calendar.timegm(time.strptime(requested_start_raw, "%Y-%m-%d"))
+    except ValueError:
+      print("WARNING: Invalid start date specified - using default (all available data)")
+      requested_start = 0
+#print(strftime("%Y-%m-%d-%H:%M:%S", gmtime(requested_start)))
+
+# Read in log file
+print("Analysing log file:  %s" % log_file)
+with open(log_file, "r") as f:
+  raw_log = list(f)
+
+# Find first switch in log (and hence earliest start time) and  end time of log
+for line in raw_log:
+  if "Switching system" in line:
+    start_time = calendar.timegm(time.strptime(line[0:10], "%Y-%m-%d")) + 86400
+    break
+end_time = calendar.timegm(time.strptime(raw_log[-1][0:10], "%Y-%m-%d"))
+print("Log file covers %s to %s" % (raw_log[0][0:19], raw_log[-1][0:19]))
+print("Log file can be analysed from from %s to %s" % (strftime("%Y-%m-%d_%H:%M:%S", gmtime(start_time)), strftime("%Y-%m-%d-%H:%M:%S", gmtime(end_time))))
+num_days = int((end_time - start_time) / 86400)
+end_time_log = end_time
+print("Total %s log lines, %d full days in log" % (len(raw_log), num_days))
+
+# Check requested end time, and set default
+if len(sys.argv) < 4:
+  requested_end = end_time
+else:
+  requested_end_raw = sys.argv[3]
+  if str.isdigit(requested_end_raw):
+    requested_end = int(requested_end_raw)
+  else:
+    try:
+      requested_end = calendar.timegm(time.strptime(requested_end_raw, "%Y-%m-%d"))
+    except ValueError:
+      print("WARNING: Invalid end date specified - using default (all available data)")
+      requested_end = end_time
+#print(strftime("%Y-%m-%d-%H:%M:%S", gmtime(requested_end)))
+
+# Determine if shorter timescale for analysis has been specified
+if requested_start > start_time and requested_start < end_time:
+  start_time = calendar.timegm(time.strptime(strftime("%Y-%m-%d", gmtime(requested_start)), "%Y-%m-%d"))
+print("Analysis start date %s" % strftime("%Y-%m-%d_%H:%M:%S", gmtime(start_time)))
+if requested_end > start_time:
+  end_time = calendar.timegm(time.strptime(strftime("%Y-%m-%d", gmtime(requested_end)), "%Y-%m-%d"))
+print("Analysis end date %s" % strftime("%Y-%m-%d_%H:%M:%S", gmtime(end_time)))
+
+# Create list of datestamps of days being analysed
+num_days_log = num_days
+num_days = int((end_time - start_time) / 86400)
+print("Analysing %d full days" % (num_days))
+if end_time > end_time_log:
+  print("WARNING: Requested analysis period ends after last log line - assuming no changes in status between these times")
+current_day = start_time
+datestamps = []
+timestamps = []
+for ii in range(0, num_days):
+  # print(ii,strftime("%Y-%m-%d-%H:%M:%S", gmtime(current_day)))
+  datestamps.append(strftime("%Y%m%d", gmtime(current_day)))
+  timestamps.append(current_day)
+  current_day += 86400
+datestamps_extra = datestamps[:]
+datestamps_extra.append(strftime("%Y%m%d", gmtime(current_day)))
+timestamps.append(current_day)
+# datestamps: list of every day analysed (string)
+# timestamps: list of day boundary of every day analysed-including start and end (unix timestamp)
+# datestamps_extra-list of every day boundary-including start and end (string)
+# print(datestamps, datestamps_extra, timestamps[0], len(timestamps))
+
+# Create status list, showing status at midnight every day
+status_list = []
+current_status = -1
+for line in datestamps_extra:
+  status_list.append("-1")
+prev_time = calendar.timegm(time.strptime(raw_log[0][0:10], "%Y-%m-%d"))
+for line in raw_log:
+  try:
+    line_time = calendar.timegm(time.strptime(line[0:19], "%Y-%m-%d-%H:%M:%S"))
+  except ValueError:
+    try:
+      line_time = calendar.timegm(time.strptime(line[0:19], "%Y-%m-%d-%H-%M-%S"))
+    except ValueError:
+      # Line does not contain valid data - ignore it
+      print("Invalid line:  %s" % repr(line))
+      continue
+  line_day = strftime("%Y%m%d", gmtime(line_time))
+  if line_day != strftime("%Y%m%d", gmtime(prev_time)):
+    # Day rollower has occurred
+    if line_time > start_time and prev_time < start_time:
+      status_start_analysis = current_status
+      #print(status_start_analysis)
+    try:
+      start_day_index = datestamps_extra.index(strftime("%Y%m%d", gmtime(prev_time+86400)))
+      end_day_index = datestamps_extra.index(line_day)
+      for ii in range(start_day_index,end_day_index+1):
+        status_list[ii] = current_status
+    except ValueError:
+      pass
+      # print(strftime("%Y%m%d", gmtime(prev_time+86400)), line_day)
+  if "Switching system on" in line:
+    current_status = 1
+  if "Switching system off" in line:
+    current_status = 0
+  prev_time = line_time
+# If last day(s) in analysis have no data in log, pad with last known status
+indices = [jj for jj, s in enumerate(raw_log) if 'Switching system' in s]
+last_status_change_line = raw_log[indices[-1]]
+if "Switching system on" in last_status_change_line:
+  status_end_analysis = 1
+elif "Switching system off" in last_status_change_line:
+  status_end_analysis = 0
+else:
+  print("ERROR: invalid last switching line: " + last_status_change_line)
+  sys.exit(1)
+if status_list[-1] == "-1":
+  index = ii+1
+  for line in status_list[ii+1:]:
+    status_list[index] = status_end_analysis
+    index += 1
+# # If first day(s) in analysis have no data in log, pad with known start status
+# first_status_change_line = raw_log[indices[0]]
+# # Inverted logic here - if first line is switching on, then assume off in all time before log
+# if "Switching system on" in first_status_change_line:
+#   status_start_analysis = 0
+# elif "Switching system off" in first_status_change_line:
+#   status_start_analysis = 1
+# else:
+#   print("ERROR: invalid first switching line: " + first_status_change_line)
+#   sys.exit(1)
+# if status_list[0] == "-1":
+#   index = 0
+#   for line in status_list:
+#     if line == "-1":
+#       status_list[index] = status_start_analysis
+#     else:
+#       break
+#     index += 1
+# print(status_list)
+
+# Step through days, reading log lines and calculating time on each day
+counter = 0
+time_on = []
+for line in datestamps:
+  todays_lines = [s for s in raw_log if (line[0:4]+"-"+line[4:6]+"-"+line[6:8]) in s]
+  #print(line,todays_lines,line[0:4]+"-"+line[4:6]+"-"+line[6:8])
+  if todays_lines:
+    current_status = status_list[counter]
+    todays_total = 0
+    if current_status == 1:
+      last_on = timestamps[counter]
+    for line in todays_lines:
+      try:
+        line_time = calendar.timegm(time.strptime(line[0:19], "%Y-%m-%d-%H:%M:%S"))
+      except ValueError:
+        try:
+          line_time = calendar.timegm(time.strptime(line[0:19], "%Y-%m-%d-%H-%M-%S"))
+        except ValueError:
+          # Line does not contain valid data - ignore it
+          continue
+      if "Switching system on" in line and current_status == 0:
+        current_status = 1
+        last_on = line_time
+      if "Switching system off" in line and current_status == 1:
+        current_status = 0
+        todays_total += line_time - last_on
+    if current_status != status_list[counter+1]:
+      print("ERROR! inconsistency between status from log lines and expected midnight status!")
+      print(current_status, status_list[counter+1], last_on, todays_total)
+    if current_status == 1:
+      todays_total += timestamps[counter+1] - last_on
+    time_on.append(todays_total)
+  else:
+    # If no data, status for entire day same as at start of day
+    time_on.append(status_list[counter] * 86400)
+  counter += 1
+#print(datestamps,time_on)
+time_on_hours = [float(x)/3600 for x in time_on]
+duty_cycle = [float(x)/864 for x in time_on]
+
+# Summary
+summary_string = "Total of %.1f hours on in %d days (mean %.2f hours/day)" %(sum(time_on_hours), num_days, sum(time_on_hours)/num_days)
+print(summary_string)
+max_hours = max(time_on_hours)
+max_index = time_on_hours.index(max_hours)
+print("Max %.1f hours in a day (on %s) and min %.1f hours in a day" %(max_hours, datestamps[max_index], min(time_on_hours)))
+file_timestamp = strftime("%Y%m%d_%H%M%S", gmtime())
+data_filename = file_timestamp + "_controller_analysis.csv"
+print("Saving csv of results to %s" %data_filename)
+with open(data_filename, "w") as f:
+  counter = 0
+  f.write("Standard Date,Date,Time ON (hours),Time ON (%)\n")
+  for line in datestamps:
+    f.write("%s,%s/%s/%s,%s,%s\n" %(line, line[6:8],line[4:6],line[0:4], "{:.2f}".format(time_on_hours[counter]), "{:.2f}".format(duty_cycle[counter])))
+    counter += 1
+print("Saving plots of results")
+
+# Create datenums for date ticks on plots
+DT_datestamps = [DT.datetime.strptime(x, "%Y%m%d") for x in datestamps]
+datenums = [date2num(x) for x in DT_datestamps]
+
+# Plot hours on bar chart
+fig, ax = plt.subplots(1)
+plt.bar(datenums, time_on_hours)
+ax.xaxis_date()
+loc = ax.xaxis.get_major_locator()
+loc.maxticks[DAILY] = 12
+plt.title(summary_string)
+plt.ylabel('Time ON each day (hours)')
+fig = matplotlib.pyplot.gcf()
+fig.set_size_inches(8,6)
+fig.autofmt_xdate(bottom=0.15)
+dateFmt = matplotlib.dates.DateFormatter('%Y-%m-%d')
+ax.xaxis.set_major_formatter(dateFmt)
+plt.savefig(file_timestamp+"_controller_log_plot_bar.png", format='png', dpi=300)
+
+# Plotting on chart
+plt.close('all')
+fig, ax = plt.subplots(1)
+plt.plot_date(datenums,time_on_hours, 'r-o')
+loc = ax.xaxis.get_major_locator()
+loc.maxticks[DAILY] = 12
+plt.title(summary_string)
+plt.ylabel('Time ON each day (hours)')
+fig.autofmt_xdate(bottom=0.15)
+dateFmt = matplotlib.dates.DateFormatter('%Y-%m-%d')
+ax.xaxis.set_major_formatter(dateFmt)
+plt.savefig(file_timestamp+"_controller_log_plot.png", format='png', dpi=300)
+
+print(strftime("%Y-%m-%d-%H:%M:%S: Completed temperature controller log analysis", gmtime()))
