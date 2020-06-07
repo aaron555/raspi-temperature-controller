1c606e1119f72c49ad87889f1f07f7ccf00c47e2
Aaron Lockton <aaronlockton@yahoo.co.uk>
2020-06-07 22:10:29 +0100

    removed hardcoding of paths in alias file

--- services/aliases-temperature-controller.sh
+++ services/aliases-temperature-controller.sh
@@ -1,4 +1,10 @@
-alias s='/opt/scripts/temperature-controller/temperature_controller.sh set'
-alias g='/opt/scripts/temperature-controller/temperature_controller.sh get'
-alias a='/opt/scripts/temperature-controller/temperature_controller.sh analyse'
-alias s3='/opt/scripts/temperature-controller/temperature_controller.sh sync'
+# Aliases for temperature controller - to be placed in /etc/profile.d
+source /etc/controller.conf
+if [[ ! -f ${SCRIPTDIR}/temperature_controller.sh ]]; then
+  # Set default location for scripts
+  ${SCRIPTDIR}=/opt/scripts/temperature-controller
+fi
+alias s="${SCRIPTDIR}/temperature_controller.sh set"
+alias g="${SCRIPTDIR}/temperature_controller.sh get"
+alias a="${SCRIPTDIR}/temperature_controller.sh analyse"
+alias s3="${SCRIPTDIR}/temperature_controller.sh sync"
